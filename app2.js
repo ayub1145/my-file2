1d92ac67a18da6de2577779050f8d795035ca13c
Md. Ayub Ali <76879092+ayub1145@users.noreply.github.com>
2021-01-02 23:40:27 +0600

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,8 @@
+document.getElementById("click_me").addEventListener("click",function(){
+	document.getElementById("sub_Menu").style.display="block";
+	
+})
+document.getElementById("demo").addEventListener("mouseover",function(){
+	document.getElementById("sub_Menu").style.display="none";
+	
+})
